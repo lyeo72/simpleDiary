935b2e81ad2bd2f4755a14da8c5b33970baeed42
lyeo72 <lyeo7272@gmail.com>
2022-05-23 04:03:18 +0900

    리스트 렌더링

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import DiaryEditer from "./DiaryEditer";
+import DiaryList from "./DiaryList";
 
+const dummyList = [
+  {
+    id: 1,
+    author: "홍길동",
+    content: "내용1",
+    emotion: 2,
+    create_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: "이순신",
+    content: "내용2",
+    emotion: 1,
+    create_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: "유여진",
+    content: "내용3",
+    emotion: 4,
+    create_date: new Date().getTime(),
+  },
+];
 function App() {
   return (
     <div className="App">
       <DiaryEditer />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
